9cc515f9b9f164f2d83cc0d3324d804da25dbcf7
Josh Roth <josh@cleanenergyexperts.com>
2014-02-15 23:12:16 -0800

    Major updates

--- source/js/home.js
+++ source/js/home.js
@@ -2,4 +2,30 @@ $(document).ready(function() {
 	$('.close').click(function() {
 		$('#aboutme').fadeOut();
 	});
+
+	var folioFilter = function(c) {
+		var categories = ['animation', 'graphicdesign', 'printdesign', 'webdesign'];
+		var filter = jQuery.grep(categories, function( a ) {
+		  return a != c;
+		});
+		if(c == 'all') {
+			$.each(categories,function(key, value) { 
+				$('.portfolio-' + value).show();
+			});
+			return false;
+		}
+		$('.portfolio-' + c).show();		
+		$.each(filter,function(key, value) { 
+			$('.portfolio-' + value).hide();
+		});
+	}
+
+	$('.filter').click(function() {
+		var category = $(this).attr('href').replace(/^#/, '');
+		folioFilter(category);
+
+		$('.active').removeClass('active');
+		$(this.parentNode).addClass('active');
+		$('#filter-replace').html($(this).text());
+	});
 });
